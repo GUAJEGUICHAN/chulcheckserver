8c2a6c9c244deb01e9f596c85fd62aa4b93744ce
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-02-18 01:52:08 +0900

    멤버 데이터

---
+++ data/allMembers.js
@@ -0,0 +1,29 @@
+const allMemebers = [
+  {
+    isStudent: true,
+    name: '이승만',
+    grade: 1,
+    class: 1,
+    attendedDates: ['220102', '220109', '220116', '220123'],
+  }, {
+    isStudent: true,
+    name: '윤보선',
+    grade: 1,
+    class: 1,
+    attendedDates: ['220102', '220109', '220116', '220123'],
+  }, {
+    isStudent: true,
+    name: '박정희',
+    grade: 1,
+    class: 1,
+    attendedDates: ['220102', '220109', '220116', '220123'],
+  }, {
+    isStudent: true,
+    name: '최규하',
+    grade: 1,
+    class: 1,
+    attendedDates: ['220102', '220109', '220116', '220123'],
+  },
+];
+
+export default allMemebers;
